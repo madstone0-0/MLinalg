0399d452ce7d8be6ff8f472cf54054120ee4ef7d
Madiba Hudson-Quansah <mhquansah@gmail.com>
2025-07-17 09:09:18 +0000

    WIP Neural Network example

---
+++ src/examples/nn.cpp
@@ -0,0 +1,13 @@
+#include <print>
+
+#include "MLinalg.hpp"
+#include "operations/Builders.hpp"
+#include "structures/Aliases.hpp"
+
+using namespace std;
+using namespace mlinalg;
+using namespace mlinalg::structures;
+
+// Example of a simple feed foward neural network using MLinalg
+
+int main() {}
