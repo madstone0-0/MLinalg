297bd018acf7a13c42e74c08dd35b218c12993c1
Madiba Hudson-Quansah <mhquansah@gmail.com>
2025-07-24 18:24:45 +0000

    Fix random builders

--- src/pub/operations/Builders.hpp
+++ src/pub/operations/Builders.hpp
@@ -196,7 +196,7 @@ namespace mlinalg {
      * @return The random vector of the given size.
      */
     template <Number number, int n>
-    Vector<number, n> vectorRandom(const double min = 0, const double max = 100, const Seed& seed = std::nullopt) {
+    Vector<number, n> vectorRandom(const number min = 0, const number max = 100, const Seed& seed = std::nullopt) {
         Vector<number, n> vec{};
         vec.apply([&](auto& x) { x = helpers::rng<number>(min, max, seed); });
         return vec;
@@ -213,7 +213,7 @@ namespace mlinalg {
      * @return The random vector of the given size.
      */
     template <Number number, int n>
-    Vector<number, n> vectorRandom(const int size, const double min = 0, const double max = 100,
+    Vector<number, n> vectorRandom(const int size, const number min = 0, const number max = 100,
                                    const Seed& seed = std::nullopt) {
         if constexpr (n == Dynamic) {
             Vector<number, n> vec(size);
@@ -235,7 +235,7 @@ namespace mlinalg {
      * @return The random matrix of the given size.
      */
     template <Number number, int m, int n>
-    Matrix<number, m, n> matrixRandom(int min = 0, int max = 100, Seed seed = std::nullopt) {
+    Matrix<number, m, n> matrixRandom(const number min = 0, const number max = 100, Seed seed = std::nullopt) {
         Matrix<number, m, n> res{};
         res.apply([&](auto& x) { x = helpers::rng<number>(min, max, seed); });
         return res;
@@ -254,8 +254,8 @@ namespace mlinalg {
      * @return The random matrix of the given size.
      */
     template <Number number, int m, int n>
-    Matrix<number, m, n> matrixRandom(const int numRows, const int numCols, const int min = 0, const int max = 100,
-                                      const Seed& seed = std::nullopt) {
+    Matrix<number, m, n> matrixRandom(const int numRows, const int numCols, const number min = 0,
+                                      const number max = 100, const Seed& seed = std::nullopt) {
         if constexpr (n == Dynamic || m == Dynamic) {
             Matrix<number, m, n> res(numRows, numCols);
             res.apply([&](auto& x) { x = helpers::rng<number>(min, max, seed); });
